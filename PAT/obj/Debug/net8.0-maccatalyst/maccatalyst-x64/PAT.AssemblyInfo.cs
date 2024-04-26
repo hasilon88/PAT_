e4a142647266e6d96a28676f35fccb5307f9eff0
Harjot Singh <hasilon88@gmail.com>
2024-04-26 14:43:32 -0400

    Conflict Fixes.
    Conflict Fixes.

--- PAT/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/PAT.AssemblyInfo.cs
+++ PAT/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/PAT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f568c9a575ab4b27788055fcd38d5dbc89839af2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
