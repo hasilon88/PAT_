923b50664366ed90e57b9aca1639bc0b69bca35a
Harjot Singh <hasilon88@gmail.com>
2024-04-26 14:44:16 -0400

    i

---
+++ PAT/Models/Entities/Enums/StudentRoleTypeEnums.cs
@@ -0,0 +1,12 @@
+﻿namespace PAT.Models.Entities.Enums
+{
+    /// <summary>
+    /// Represents different student role types.
+    /// </summary>
+    public enum StudentRoleTypeEnums
+    {
+        None = 0,
+        Tutor = 1,
+        Tutee = 2,
+    }
+}
